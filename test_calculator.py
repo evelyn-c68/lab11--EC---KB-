6cff8c039725e4b5b112352e5343d55ef9fc2359
kayleebleeker15 <kayleebleeker@ufl.edu>
2026-04-09 17:20:56 +0000

    modified test calculator

--- test_calculator.py
+++ test_calculator.py
@@ -1,14 +1,23 @@
+#https://github.com/evelyn-c68/lab11--EC---KB-.git
+# Partner 1: Evelyn Chen
+# Partner 2: Kaylee Bleeker
+
 import unittest
-from calculator import *
+from calculator import add, sub, mul, div, log, exp
 
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
+    def test_add(self): # 3 assertions
+        self.assertEqual(add(0,0),0)
+        self.assertEqual(add(-2,2),0)
+        self.assertEqual(add(2,3),5)
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_subtract(self): # 3 assertions
+        self.assertEqual(sub(4,2),2)
+        self.assertEqual(sub(0,5),-5)
+        self.assertEqual(sub(3,3),0)
+
+    ##########################
 
     ######## Partner 1
     # def test_multiply(self): # 3 assertions
@@ -19,18 +28,20 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
+    def test_divide_by_zero(self): # 1 assertion
+        # call division function inside, example:
+        with self.assertRaises(ZeroDivisionError):
+            div(0, 5)
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
+    def test_logarithm(self): # 3 assertions
+        self.assertEqual(log(1, 100),2)
+        self.assertEqual(log(2,4),2)
+        self.assertEqual(log(3,27),3)
 
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
+    def test_log_invalid_base(self): # 1 assertion
+        # use same technique from test_divide_by_zero
+        with self.assertRaises(ValueError):
+            log(0,10) #base cannot 0
     # ##########################
     
     ######## Partner 1
